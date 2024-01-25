12e84494c72062ef641e9ab2c14006c26a6ebfaa
lucapolzone <lucapolzone@gmail.com>
2024-01-25 08:29:08 +0100

    feat: add snack 01

--- snack-01/main.js
+++ snack-01/main.js
@@ -1,1 +1,27 @@
-console.log("js ok")
+const vips = [
+  'Dwayne Johnson',
+  'Brad Pitt',
+  'Johnny Depp',
+  'Lady Gaga',
+  'Cristiano Ronaldo',
+  'Georgina Rodriguez',
+  'Chiara Ferragni',
+  'Fedez',
+  'George Clooney',
+  'Amal Clooney',
+  'Maneskin',
+];
+
+
+
+const newArray = vips.map((nome, index) => {
+  return {
+    tavolo: "Tavolo vip",
+    nome: nome,
+    posto: index + 1
+  }
+});
+
+console.log(newArray);
+
+
