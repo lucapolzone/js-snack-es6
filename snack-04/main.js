b17dfefa23778c4ba9164f11429d70173488c607
lucapolzone <lucapolzone@gmail.com>
2024-01-28 16:22:16 +0100

    feat: add snack 4

---
+++ snack-04/main.js
@@ -0,0 +1,61 @@
+const containerElement = document.getElementById("container")
+
+const teams = [
+  {
+    name: 'Team Turtle',
+    score: 0,
+    foul: 0,
+  },
+  {
+    name: 'Team Frog',
+    score: 0,
+    foul: 0,
+  },
+  {
+    name: 'Team Penguin',
+    score: 0,
+    foul: 0,
+  },
+  {
+    name: 'Team Hippopotamus',
+    score: 0,
+    foul: 0,
+  },
+  {
+    name: 'Team Seal',
+    score: 0,
+    foul: 0,
+  },
+  {
+    name: 'Team Crocodile',
+    score: 0,
+    foul: 0,
+  },
+  {
+    name: 'Team Dolphin',
+    score: 0,
+    foul: 0,
+  },
+];
+
+
+for (let team of teams) {
+  team.score = getRandomNumber(1, 20);
+  team.foul = getRandomNumber(1, 20);
+}
+
+
+const arrayFouls = teams.map((team) => {
+//const {name, foul, score} = team;
+  const {name, foul} = team;
+//return {name, foul, score};
+  return {name, foul};
+})
+
+console.log(arrayFouls);
+
+
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * ((max + 1) - min) + min); 
+}
